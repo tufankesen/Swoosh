1f8fbb05bb0e7c83474e84accfb5961ef24de583
Tufan Kesen <tufankesen@gmail.com>
2020-11-01 22:51:14 +0300

    lesson 4

--- app/src/main/java/com/tufankesen/swoosh/LeagueActivity.kt
+++ app/src/main/java/com/tufankesen/swoosh/LeagueActivity.kt
@@ -3,7 +3,7 @@ package com.tufankesen.swoosh
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 
-class LeagueActivity : AppCompatActivity() {
+class LeagueActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_league)
