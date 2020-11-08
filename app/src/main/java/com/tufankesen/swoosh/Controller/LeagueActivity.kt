14a5a9f84f226fd91b0ef7ce01e6a729336b478c
Tufan Kesen <tufankesen@gmail.com>
2020-11-08 16:19:02 +0300

    lesson 10

--- app/src/main/java/com/tufankesen/swoosh/Controller/LeagueActivity.kt
+++ app/src/main/java/com/tufankesen/swoosh/Controller/LeagueActivity.kt
@@ -2,6 +2,7 @@ package com.tufankesen.swoosh.Controller
 
 import android.content.Intent
 import android.os.Bundle
+import android.os.PersistableBundle
 import android.view.View
 import android.widget.Toast
 import com.tufankesen.swoosh.Utilities.EXTRA_PLAYER
@@ -12,6 +13,19 @@ import com.tufankesen.swoosh.Utilities.EXTRA_PLAYER
 
 class LeagueActivity : BaseActivity() {
     var player = Player("", "")
+
+    override fun onSaveInstanceState(outState: Bundle?) {
+        super.onSaveInstanceState(outState)
+        outState?.putParcelable(EXTRA_PLAYER, player)
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle?) {
+        super.onRestoreInstanceState(savedInstanceState)
+        if(savedInstanceState != null)
+        {
+            player = savedInstanceState.getParcelable(EXTRA_PLAYER)
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
--- app/src/main/java/com/tufankesen/swoosh/Controller/SkillActivity.kt
+++ app/src/main/java/com/tufankesen/swoosh/Controller/SkillActivity.kt
@@ -12,6 +12,19 @@ import kotlinx.android.synthetic.main.activity_skill.*
 class SkillActivity : BaseActivity() {
     lateinit var player : Player
 
+    override fun onSaveInstanceState(outState: Bundle?) {
+        super.onSaveInstanceState(outState)
+        outState?.putParcelable(EXTRA_PLAYER, player)
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle?) {
+        super.onRestoreInstanceState(savedInstanceState)
+        if(savedInstanceState != null)
+        {
+            player = savedInstanceState.getParcelable(EXTRA_PLAYER)
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_skill)
