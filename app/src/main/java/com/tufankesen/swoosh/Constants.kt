9ac2a033f8183a484cd49bfdb17a5f31aff86afb
Tufan Kesen <tufankesen@gmail.com>
2020-11-08 12:21:16 +0300

    lesson-07

---
+++ app/src/main/java/com/tufankesen/swoosh/Constants.kt
@@ -0,0 +1,3 @@
+package com.tufankesen.swoosh
+
+const val EXTRA_LEAGUE = "league"
--- app/src/main/java/com/tufankesen/swoosh/SkillActivity.kt
+++ app/src/main/java/com/tufankesen/swoosh/SkillActivity.kt
@@ -2,10 +2,16 @@ package com.tufankesen.swoosh
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_skill.*
 
 class SkillActivity : BaseActivity() {
+
+    var league = ""
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_skill)
+        league = intent.getStringExtra(EXTRA_LEAGUE)
+        println(league)
     }
 }
